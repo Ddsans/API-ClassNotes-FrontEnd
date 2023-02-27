adfb0a95513b1ccd8d5ca7a1871a2d43c6bdf931
David dos Santos <100370726+Ddsans@users.noreply.github.com>
2023-02-26 23:30:56 -0300

    correção caminho

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import {RiShutDownLine} from 'react-icons/ri';
 import { useAuth } from '../../hooks/auth';
 
 import { api } from '../../services/api';
-import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+import avatarPlaceholder from "../../Assets/avatar_placeholder.svg";
 
 import { Container, Profile, Logout } from "./style";
 
