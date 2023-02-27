6cdbc6f6a29c5679219ad864b8e1469ce0961829
David dos Santos <100370726+Ddsans@users.noreply.github.com>
2023-02-26 22:44:36 -0300

    Primeira versão

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,43 @@
+import { useNavigate } from 'react-router-dom';
+
+import {RiShutDownLine} from 'react-icons/ri';
+import { useAuth } from '../../hooks/auth';
+
+import { api } from '../../services/api';
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+
+import { Container, Profile, Logout } from "./style";
+
+export function Header() {
+  const { signOut, user } = useAuth();
+  const navigation = useNavigate();
+
+  function handleSignOut(){
+    navigation("/");
+    signOut();
+  }
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+
+  return(
+    <Container>
+      <Profile to="/profile">
+        <img 
+          src={avatarUrl}
+          alt="Foto do usuário"
+          />
+
+          <div>
+            <span>Bem-vindo</span>
+            <strong>{user.name}</strong>
+          </div>
+      </Profile>
+
+      <Logout onClick={handleSignOut}>
+        <RiShutDownLine/>
+      </Logout>
+
+    </Container>
+  );
+}
---
+++ src/hooks/auth.jsx
@@ -0,0 +1,100 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+import { api } from '../services/api'
+
+export const AuthContext = createContext({});
+
+function AuthProvider({children}) {
+  const [data, setData] = useState({});
+  
+  async function signIn({ email, password}) {
+
+    try{
+      const response = await api.post("/sessions", { email, password});
+      const { user, token } = response.data;
+
+      localStorage.setItem("@classnotes:user", JSON.stringify(user));
+      localStorage.setItem("@classnotes:token", token);
+
+      api.defaults.headers.common['Authorization']= `Bearer ${token}`
+      
+      setData({user, token});
+
+    } catch(error) {
+      if(error.response){
+        alert(error.response.data.message);
+      } else{
+        alert("Não foi possível entrar.");
+      }
+    }
+
+  }
+
+  function signOut(){
+    localStorage.removeItem("@classnotes:token");
+    localStorage.removeItem("@classnotes:user");
+
+    setData({});
+
+  }
+
+  async function updateProfile({ user, avatarFile }) {
+    try{
+
+      if(avatarFile){
+        const fileUploadForm = new FormData();
+        fileUploadForm.append("avatar", avatarFile);
+
+        const response = await api.patch("/users/avatar", fileUploadForm)
+        user.avatar = response.data.avatar;
+      }
+
+      await api.put("/users", user);
+      localStorage.setItem("@classnotes:user", JSON.stringify(user))
+
+      setData({ user, token: data.token });
+      alert("Perfil atualizado!")
+
+    } catch(error) {
+      if(error.response){
+        alert(error.response.data.message);
+      } else{
+        alert("Não foi possível atualizar o perfil.");
+      }
+    }
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@classnotes:token");
+    const user = localStorage.getItem("@classnotes:user");
+
+    if(token && user) {
+      api.defaults.headers.common['Authorization']= `Bearer ${token}`
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ 
+      signIn, 
+      user: data.user, 
+      signOut,
+      updateProfile,
+     }}
+      >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth(){
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export {AuthProvider, useAuth};
